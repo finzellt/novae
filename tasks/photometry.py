f2bedc17f5137705da90a19713c61171fa8236a9
MaxMorehead <moreheadmax@gmail.com>
2016-07-25 13:00:02 -0400

    fixed photometry task

--- tasks/photometry.py
+++ tasks/photometry.py
@@ -1,3 +1,4 @@
+from ..nova import NOVA
 from astrocats.catalog.utils import pbar
 import os
 import os.path
@@ -5,8 +6,6 @@ from ..utils import read_photometry_ticket, get_nova_name, convert_date_UTC
 import re
 import csv
 from astropy.time import Time
-
-from ..nova import NOVA
 
 
 def do_photometry(catalog):
@@ -19,14 +18,17 @@ def do_photometry(catalog):
 	
 	for filename in pbar(os.listdir(ticket_directory), task_str):
 		raw_dict, column_dict = {}, {}		
+		
 		if not re.search(r'photometry', filename.lower()):
 			continue
 		with open(os.path.join(ticket_directory, filename)) as phot_ticket:
 			try:			
 				raw_dict, column_dict = read_photometry_ticket(phot_ticket.read())
 				phot_ticket.close()
-			except AttributeError:
+			except:
 				True == True
+				#print(filename)
+				continue
 				#THROW ERROR TO LOG HERE
 
 		nova_name = get_nova_name(raw_dict['OBJECT NAME'])
@@ -35,29 +37,31 @@ def do_photometry(catalog):
 	
 		name = nova_name.replace('_', ' ')
 		name = catalog.add_entry(name)
-		source = catalog.entries[name].add_source(bibcode=raw_dict['BIBCODE'], reference=raw_dict['REFERENCE'])
+		if type(raw_dict['BIBCODE']) is str and len(bibcode) == 19:
+			source = catalog.entries[name].add_source(bibcode=raw_dict['BIBCODE'], reference=raw_dict['REFERENCE'])
+		else:
+			source = catalog.entries[name].add_source(bibcode='1234567890123456789', reference=raw_dict['REFERENCE'])
 		catalog.entries[name].add_quantity(NOVA.ALIAS, name, source)
 		
 		csvfile = open(file_path)
-		contents = csv.reader(csvfile, delimiter=',', quotechar='"')
-		csvfile.close()
+		contents = list(csv.reader(csvfile, delimiter=',', quotechar='"'))
 		
 		
-		time_offset = 0		
+		
+		time_offset = 0
 		if re.match(r'dpo', raw_dict['TIME UNITS'].lower()):
 			t = Time(convert_date_UTC(raw_dict['ASSUMED DATE OF OUTBURST']))
 			offset = t.jd 
 
-		column_corr = {'TIME COLUMN': 'time', 'FILTER/FREQUENCY/ENERGY RANGE': 'band', 'TELESCOPE': 'telescope', 'OBSERVER': 'observer', 'FILTER SYSTEM': None, 'UPPER LIMIT FLAG': None}
+		column_corr = {'TIME': 'time', 'FILTER/FREQUENCY/ENERGY RANGE': 'band', 'TELESCOPE': 'telescope', 'OBSERVER': 'observer', 'FILTER SYSTEM': None, 'UPPER LIMIT FLAG': None}
 
-		if re.search(r'mag', raw_dict['FLUX UNITS'].lower()): column_corr['FLUX COLUMN'] = 'magnitude'
-		else: column_corr['FLUX COLUMN'] = 'flux'
+		if re.search(r'mag', raw_dict['FLUX UNITS'].lower()): column_corr['FLUX'] = 'magnitude'
+		else: column_corr['FLUX'] = 'flux'
 
 		if re.search(r'mag', raw_dict['FLUX ERROR UNITS'].lower()): column_corr['FLUX ERROR'] = 'e_magnitude'
 		else: column_corr['FLUX ERROR'] = 'e_flux'
 
-
-		for line in contents:
+		for line in pbar(contents, name):
 			data_dict = {}
 			line = [value.strip() for value in line]
 			if line[0].startswith("#"): continue
@@ -65,18 +69,30 @@ def do_photometry(catalog):
 			for key in column_dict:
 				if column_dict[key] is None:
 					continue
-				data_dict[column_corr[key]] = line[column_dict[key]]
+				if line[column_dict[key]] != '':
+					data_dict[column_corr[key]] = line[column_dict[key]]
 				
 			#if re.match(r'ut', raw_dict['TIME UNITS'].lower()):
 			#	data_dict['time'] = (Time(data_dict)).jd
 			#data_dict['time'] += offset
 			if None in data_dict: del data_dict[None]
 			
-			data_dict['source'] = source
+			
 			if 'telescope' not in data_dict: data_dict['telescope'] = raw_dict['TELESCOPE']
 			if 'observer' not in data_dict: data_dict['observer'] = raw_dict['OBSERVER']
-
+			
+			key_list = list(data_dict.keys())
+			for key in key_list:
+				if data_dict[key] is None:
+					del data_dict[key]
+			
+			
+			
+			data_dict['source'] = source
+			
 			catalog.entries[name].add_photometry(**data_dict)
+		csvfile.close()
 		catalog.journal_entries()
+		
 	return
 			
--- utils/readtickets.py
+++ utils/readtickets.py
@@ -1,34 +1,34 @@
 import re
 
-__all__ = ['read_photometry_ticket', 'read_spectra_ticket','read_spectra_metadata']
-
+__all__ = ['read_photometry_ticket', 'read_spectra_ticket', 'read_spectra_metadata']
 
 def read_photometry_ticket(contents):
 	""" Reads photometry ticket
-		Returns tuple of dictionaries (value_dict, column_dict
+		Returns tuple of dictionaries (value_dict, column_dict)
 	"""	
 
-	value_dict = {}, column_dict
+	value_dict, column_dict = {}, {}
 	ticket_fields = ["OBJECT NAME: ", "TIME UNITS: ", "FLUX UNITS: ",  "FLUX ERROR UNITS: ", "FILTER SYSTEM: ", "MAGNITUDE SYSTEM: ", "WAVELENGTH REGIME: ", "TIME SYSTEM: ", "ASSUMED DATE OF OUTBURST: ", "TELESCOPE: ", "OBSERVER: ", "REFERENCE: ", "BIBCODE: ", "DATA FILENAME: ", "TIME COLUMN NUMBER: ", "FLUX COLUMN NUMBER: ", "FLUX ERROR COLUMN NUMBER: ", "FILTER/FREQUENCY/ENERGY RANGE COLUMN NUMBER: ", "UPPER LIMIT FLAG COLUMN NUMBER: ", "TELESCOPE COLUMN NUMBER: ", "OBSERVER COLUMN NUMBER: ", "FILTER SYSTEM COLUMN NUMBER: ", "TICKET STATUS: "]
 
 	lines = contents.split("\n")
-	lines = [line.split(",") for line in lines]
+
 	j = 0
 	for i in range(len(lines)):
 		name_match = re.match(ticket_fields[j], lines[i])
 		if name_match:
 			value = lines[i][name_match.end():].strip()
-			key = ticketfields[j][:-2]
+			key = ticket_fields[j][:-2]
 			if key.endswith(' COLUMN NUMBER'):
 				key = key[:-14]
 				try: column_dict[key] = int(value)
 				except ValueError: column_dict[key] = None
 			else:
-				value_dict[key] = value
+				if value.upper() == 'NA' or value == '': value_dict[key] = None
+				else: value_dict[key] = value
 		j+=1
 	if len(value_dict) + len(column_dict) != len(ticket_fields):
 		raise AttributeError('Not a valid ticket.')
-
+	
 	return (value_dict, column_dict)
 
 
@@ -39,7 +39,7 @@ def read_spectra_ticket(contents):
 	ticket_fields = ["OBJECT NAME: ", "FLUX UNITS: ", "FLUX ERROR UNITS: ", "WAVELENGTH REGIME: ", "TIME SYSTEM: ", "ASSUMED DATE OF OUTBURST: ", "REFERENCE: ", "BIBCODE: ", "DEREDDENED FLAG: ", "METADATA FILENAME: ", "FILENAME COLUMN: ", "WAVELENGTH COLUMN: ", "FLUX COLUMN: ", "FLUX ERROR COLUMN: ", "FLUX UNITS COLUMN: ", "DATE COLUMN: ", "TELESCOPE COLUMN: ", "INSTRUMENT COLUMN: ", "OBSERVER COLUMN: ", "SNR COLUMN: ", "DISPERSION COLUMN: ", "RESOLUTION COLUMN: ", "WAVELENGTH RANGE COLUMN: ", "TICKET STATUS: "]
 
 	lines = contents.split("\n")
-	lines = [line.split(",") for line in lines]
+
 	j = 0
 	for i in range(len(lines)):
 		name_match = re.match(ticket_fields[j], lines[i])
@@ -83,7 +83,7 @@ def read_spectra_metadata(contents, col_num_dict):
 			break
 
 	file_list = []
-	for i in range(k,len(lines)):
+	for i in range(k, len(lines)):
 		value_dict, column_dict = {}, {}
 		for field_name in col_num_dict:
 			if col_num_dict[field_name] is None: continue
